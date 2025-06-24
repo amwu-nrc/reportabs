74faa9e0ec1f1033c57037857bcf0b345ed9e5a9
Hamish Gamble <hamish.gamble@amwu.org.au>
2025-06-24 16:24:34 +1000

    Update colours

--- R/colour_scale.R
+++ R/colour_scale.R
@@ -1,25 +1,4 @@
-#' AITI Colour Scale
-#'
-#' @param palette Name of colour palette
-#' @param discrete TRUE for a discrete scale
-#' @param reverse TRUE to reverse the order of the colours
-#'
-#' @return ggplot2 colour scale
-#' @export
-#' @description
-#' `r lifecycle::badge("deprecated")`
-#' `scale_colour_aiti()` has been renamed `scale_colour_fof()`
-
-scale_colour_aiti <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
-
-  lifecycle::deprecate_warn("0.0.2", "scale_colour_aiti()", "scale_colour_fof()")
-
-  scale_colour_fof(palette,
-                   discrete,
-                   reverse)
-}
-
-#' Colour scale for Factory of the Future colours
+#' Colour Scale for Factory of the Future
 #'
 #' @param palette Name of the colour palette (default "main")
 #' @param discrete Whether the colours should be discrete or continuous (default TRUE)
@@ -27,47 +6,67 @@ scale_colour_aiti <- function(palette = "main", discrete = TRUE, reverse = FALSE
 #'
 #' @returns ggplot2 colour scale
 #' @export
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#' `scale_colour_fof()` has been renamed `scale_colour_nrc()`
+#'
+#'
+scale_colour_fof <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
+
+  lifecycle::deprecate_warn("0.0.3", "scale_colour_aiti()", "scale_colour_nrc()")
+
+  scale_colour_nrc(palette,
+                   discrete,
+                   reverse)
+
+}
+
+#' Colour Scale for Australian Manufacturing Workers Union
+#'
+#' @param palette Name of the colour palette (default "main")
+#' @param discrete Whether the colours should be discrete or continuous (default TRUE)
+#' @param reverse Whether the order of the colours should be reversed (default FALSE)
 #'
 #' @examples
 #' library(ggplot2)
 #' p <- ggplot(mtcars, aes(x = mpg, y = disp, col = factor(cyl))) + geom_point()
-#' p + scale_colour_fof()
-#'
-scale_colour_fof <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
+#' p + scale_colour_nrc()
 
-  pal <- fof_pal(palette = palette, reverse = reverse)
+scale_colour_nrc <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
+
+  pal <- nrc_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
-    ggplot2::discrete_scale("colour", name = paste0("fof", palette), palette = pal)
+    ggplot2::discrete_scale("colour", name = paste0("nrc", palette), palette = pal)
   } else {
     ggplot2::scale_colour_gradientn(colours = pal(256))
   }
 
 }
 
-#' AITI Fill Scale
+
+#' Fill scale for Factory of the Future colours
 #'
-#' @param palette Name of colour palette
-#' @param discrete TRUE for a discrete scale
-#' @param reverse TRUE to reverse the order of the colours
+#' @param palette Name of the colour palette (default "main")
+#' @param discrete Whether the colours should be discrete or continuous (default TRUE)
+#' @param reverse Whether the order of the colours should be reversed (default FALSE)
 #'
 #' @returns ggplot2 fill scale
 #' @export
-#'
 #' @description
 #' `r lifecycle::badge("deprecated")`
-#' `scale_fill_aiti()` has been renamed `scale_fill_fof()`
+#' `scale_fill_fof()` has been renamed `scale_fill_nrc()`
+#'
+scale_fill_fof <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
+  lifecycle::deprecate_warn("0.0.3", "scale_colour_fof()", "scale_colour_nrc()")
 
-scale_fill_aiti <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
+  scale_fill_nrc(palette,
+                 discrete,
+                 reverse)
 
-  lifecycle::deprecate_warn("0.0.2", "scale_colour_aiti()", "scale_colour_fof()")
-
-  scale_fill_fof(palette,
-                   discrete,
-                   reverse)
 }
 
-#' Fill scale for Factory of the Future colours
+#' Fill scale for Australian Manufacturing Workers Union colours
 #'
 #' @param palette Name of the colour palette (default "main")
 #' @param discrete Whether the colours should be discrete or continuous (default TRUE)
@@ -80,13 +79,12 @@ scale_fill_aiti <- function(palette = "main", discrete = TRUE, reverse = FALSE) 
 #' library(ggplot2)
 #' df <- data.frame(x = c("One", "Two", "Three"), y = c(4, 2, 9))
 #' p <- ggplot(df, aes(x = x, y = y, fill = x)) + geom_col()
-#' p + scale_fill_fof()
-scale_fill_fof <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
-
-  pal <- fof_pal(palette = palette, reverse = reverse)
+#' p + scale_fill_nrc()
+scale_fill_nrc <- function(palette = "main", discrete = TRUE, reverse = FALSE) {
+  pal <- nrc_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
-    ggplot2::discrete_scale("fill", name = paste0("fof", palette), palette = pal)
+    ggplot2::discrete_scale("fill", name = paste0("nrc", palette), palette = pal)
   } else {
     ggplot2::scale_fill_gradientn(colours = pal(256))
   }
--- data-raw/internal.R
+++ data-raw/internal.R
@@ -1,30 +1,23 @@
-# code to prepare `fof_colours` dataset goes here
-fof_colours <- c(
-  `Summer Red` = "#Dc5300",
-  `Flinders Gold` = "#FFd300",
-  `Midnight Navy` = "#232D4b",
+# code to prepare `nrc_colours` dataset goes here
+nrc_colours <- c(
+  `AMWU Orange` = "#FF5800",
+  `AMWU Blue` = "#001D26",
+  `Website Yellow` = "#ffe000",
+  `Website Grey` = "#424242",
   `Sand` = "#F6EEE1",
-  `Neon Purple` = "#8000FF",
-  `Sustainable Green` = "#4AEAB2",
-  `Soft Black` = "#2B2228",
-  `Laurel Green` = "#4D5C5A",
-  `Concrete Grey` = "#99A2A5",
   `Just White` = "#FFFFFF",
-  `Hard Black` = "#000000",
-  `Legacy Blue` = "#0B396D",
-  `Legacy Blue Highlight` = "#0077FF"
+  `Soft Black` = "#2B2228",
+  `Hard Black` = "#000000"
 )
 
 
 
 fof_palettes <- list(
-  `main` = fof_colours[c("Summer Red", "Soft Black", "Laurel Green", "Sand", "Midnight Navy")],
-  `flinders` = fof_colours[c("Flinders Gold",  "Laurel Green", "Concrete Grey")],
-  `blue` = fof_colours[c("Midnight Navy", "Soft Black", "Laurel Green", "Concrete Grey")],
-  `passive` = fof_colours[c("Soft Black", "Laurel Green", "Concrete Grey", "Sand")],
-  `just orange` = fof_colours[c("Summer Red", "Just White")],
-  `just blue` = fof_colours[c("Midnight Navy", "Sand")],
-  `legacy` = fof_colours[c("Legacy Blue", "Legacy Blue Highlight")]
+  `main` = nrc_colours[c("AMWU Orange", "Soft Black", "Sand", "AMWU Blue")],
+  `amwu` = nrc_colours[c("AMWU Orange",  "AMWU Blue", "Website Grey")],
+  `website` = nrc_colours[c("Website Yellow", "Website Grey", "Just White")],
+  `just orange` = nrc_colours[c("AMWU Orange", "Just White")],
+  `just blue` = nrc_colours[c("AMWU Blue", "Sand")]
 )
 
 usethis::use_data(fof_palettes, fof_colours, internal = TRUE, overwrite = TRUE)
